9b63a24e0d99b574a8b38ebc85b37a63d1ce523c
Amadeo Ramirez <ramirez.amadeo1@gmail.com>
2021-11-22 00:11:47 -0800

    first gpio test

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const gpio = require('onoff').Gpio;
-const dPin = 22;
-const lPin = 27;
-const cPin = 17;
+const dPin = new gpio(22, 'out');
+const lPin = new gpio(27, 'out');
+const cPin = new gpio(17, 'out');
 
 const sendShift = (data, clock, order, value) => {
   for (let i = 0; i < 8; i++) {
+    clock.writeSync(0);
+
     if (order === "be") {
-      console.log(((value << i) & 0x80) >> 7);
+      data.writeSync(((value << i) & 0x80) >> 7);
     } else if (order === "le") {
-      console.log((value >> i) & 0x01);
+      data.writeSync((value >> i) & 0x01);
     }
+
+    clock.writeSync(1);
   }
 }
 
-console.log("Big Endian: ");
+lPin.writeSync(0);
+console.log("Big Endian... ");
 sendShift(0,0,'be',0x33);
-console.log("Little Endian: ");
-sendShift(0,0,'le',0x33);
+// console.log("Little Endian... ");
+// sendShift(0,0,'le',0x33);
+lPin.writeSync(1);
+
+dPin.unexport();
+lPin.unexport();
+cPin.unexport();
