8a84e4b8d3051a529d6554520a3794e9852a5cb0
Amadeo Ramirez <ramirez.amadeo1@gmail.com>
2021-11-22 17:57:53 -0800

    basic pattern for boilerplate

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const gpio = require('onoff').Gpio;
-const {LE,BE,sleep,sendShift} = require('./utils')
+const {LE,BE,sleep,sendShift} = require('./utils');
 
 const main = async () => {
 	let i = 0;
@@ -12,7 +12,7 @@ const main = async () => {
 	const lPin = new gpio(27, 'out');
 	const cPin = new gpio(17, 'out');
 
-	// Define stop process, EOP
+	// Define stop process 'ctrl+c', EOP
 	process.on('SIGINT', function() {
 		console.log("SIGINT detected, cleaning up...");
 		dPin.unexport();
@@ -24,43 +24,12 @@ const main = async () => {
 
 	// Main loop
 	while(true){
-
-		console.log("Big Endian...");
-		for (i = 0; i < 10; i++) {
-			sendShift(dPin, cPin, lPin, BE, pattern1[0]);
-	    await sleep(500);
-			pattern1 = [pattern1[1], pattern1[0]];
+		for (i = 0; i < 7; i++) {
+			sendShift(dPin, cPin, lPin, BE, 0x01 << k);
 		}
 
-		for (i = 0; i < 10; i++) {
-			for (k = 0; k < 8; k++) {
-				sendShift(dPin, cPin, lPin, BE, 0x01 << k);
-	  	  await sleep(60);
-			}
-		}
-
-		for (i = 0; i < 10; i++) {
-			sendShift(dPin, cPin, lPin, BE, 0xAA >> i % 2);
-  	  await sleep(500);
-		}
-
-		console.log("Little Endian");
-		for (i = 0; i < 10; i++) {
-			sendShift(dPin, cPin, lPin, LE, 0xAA >> i % 2);
-  	  await sleep(500);
-		}
-
-		for (i = 0; i < 10; i++) {
-			for (k = 0; k < 8; k++) {
-				sendShift(dPin, cPin, lPin, LE, 0x01 << k);
-	  	  await sleep(60);
-			}
-		}
-
-		for (i = 0; i < 10; i++) {
-			sendShift(dPin, cPin, lPin, LE, pattern1[0]);
-	    await sleep(500);
-			pattern1 = [pattern1[1], pattern1[0]];
+		for (i = 0; i < 7; i++) {
+			sendShift(dPin, cPin, lPin, LE, 0x01 << k);
 		}
 	}
 
