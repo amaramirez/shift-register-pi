1431c4fbd5c2bef94edff021c370d9807dda7886
Amadeo Ramirez <ramirez.amadeo1@gmail.com>
2021-11-22 21:26:26 -0800

    more optitmized loop

--- main.js
+++ main.js
@@ -1,26 +1,52 @@
 const gpio = require('onoff').Gpio;
 const {LE,BE,sleep,sendShift} = require('./utils');
 
+// Define stop process 'ctrl+c', EOP
+process.on('SIGINT', function() {
+	console.log("SIGINT detected, cleaning up...");
+	ePin.writeSync(0);
+	dPin.unexport();
+	lPin.unexport();
+	cPin.unexport();
+	ePin.unexport();
+	console.log("Done.");
+	process.exit();
+});
+
+// Before main loop, setup
+const dPin = new gpio(22, 'out');
+const lPin = new gpio(27, 'out');
+const cPin = new gpio(17, 'out');
+const ePin = new gpio(4, 'out');
+
+let i = 0;
+
+const mainLoop = () => {
+	i++;
+	i%=8;
+	lPin.writeSync(0);
+	sendShift(dPin,cPin,BE,0x01 << i);
+	lPin.writeSync(1);
+}
+
+setInterval(mainLoop, 500);
+
+
+
+
+
+
+
+
+
+
+
+
 const main = async () => {
 	let i = 0;
 	let k = 0;
 
 	let pattern1 = [0xC3, 0x3C];
-
-	// Before main loop, setup
-	const dPin = new gpio(22, 'out');
-	const lPin = new gpio(27, 'out');
-	const cPin = new gpio(17, 'out');
-
-	// Define stop process 'ctrl+c', EOP
-	process.on('SIGINT', function() {
-		console.log("SIGINT detected, cleaning up...");
-		dPin.unexport();
-		lPin.unexport();
-		cPin.unexport();
-		console.log("Done.");
-		process.exit();
-	});
 
 	// Main loop
 	while(true){
